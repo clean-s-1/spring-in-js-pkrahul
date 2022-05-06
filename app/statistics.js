66d1c3051e6595f89d7878e6c36398d7b36bd88c
pkrahul <105033053+pkrahul@users.noreply.github.com>
2022-05-06 18:50:00 +0530

    added all methods and classes

--- app/statistics.js
+++ app/statistics.js
@@ -1,40 +1,51 @@
-export function getStatistics(numbers){
+module.exports = function getStatistics(numbers){
     var total = 0;
-    for( i in numbers){
-        total+=i;
+    var ret = {};
+    if (numbers.length > 0) {
+        for (i in numbers) {
+            total += i;
+        }
+         ret = {
+            min: Math.min(...numbers),
+            max: Math.max(...numbers),
+            average: total / numbers.length
+        }
     }
-    var ret = {
-        min:Math.min(...numbers),
-        max:Math.max(...numbers),
-        average:total/numbers.length
+    else {
+        ret = {
+            min: '',
+            max: '',
+            average: ''
+        }
     }
     return ret;
     //implement the computation of statistics here
 }
-export class EmailAlert{
-constructor(){
-    this.emailSent=false;
-}
-}
 
-export class LEDAlert{
-constructor(){
-    this.ledGlows=false;
+module.exports = class EmailAlert {
+    constructor() {
+        this.emailSent = false;
     }
 }
 
-export class StatsAlerter{
+module.exports = class LEDAlert {
+    constructor() {
+        this.ledGlows = false;
+    }
+}
 
-constructor(maxThreshold,alerters){
-    this.maxThreshold=maxThreshold;
-    this.alerters=alerters;
+module.exports = class StatsAlerter {
+
+    constructor(maxThreshold, alerters) {
+        this.maxThreshold = maxThreshold;
+        this.alerters = alerters;
     }
 
-checkAndAlert(nums){
-    var maximum = Math.max(...nums);
-    if(maximum > this.maxThreshold){
-        this.alerters[0].emailSent=true;
-        this.alerters[1].ledGlows=true;
+    checkAndAlert(nums) {
+        var maximum = Math.max(...nums);
+        if (maximum > this.maxThreshold) {
+            this.alerters[0].emailSent = true;
+            this.alerters[1].ledGlows = true;
         }
     }
 }
