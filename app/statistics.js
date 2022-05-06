bfc04fd1f20c6049740ca7c940f5c7509b03f431
pkrahul <105033053+pkrahul@users.noreply.github.com>
2022-05-06 16:03:09 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,36 @@
 module.exports=function getStatistics(numbers){
-    
+    var total = 0;
+    for( i in numbers){
+        total+=i;
+    }
+    var ret = {
+        min:Math.min(...numbers),
+        max:Math.max(...numbers),
+        average:total/numbers.length
+    }
+    return ret;
     //implement the computation of statistics here
 }
-
+module.exports=class EmailAlert{
+constructor(){
+    this.emailSent=false;
+}
+}
+module.exports=class LEDAlert{
+constructor(){
+    this.ledGlows=false;
+}
+}
+module.exports=class StatsAlerter{
+constructor(maxThreshold,alerters){
+this.maxThreshold=maxThreshold;
+this.alerters=alerters;
+}
+checkAndAlert(nums){
+var maximum = Math.max(...nums);
+if(maximum > this.maxThreshold){
+    this.alerters[0].emailSent=true;
+    this.alerters[1].ledGlows=true;
+}
+}
+}
